a9e5316ce4dd11575895c638838b0ea9dee3bfb7
Tomlibrary <134924914+Tomlibrary@users.noreply.github.com>
2023-05-29 12:14:14 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+	name= "TOM",
+	version= "0.1",
+	author= "TOM",
+	description= "With this library, you can search and download from Akwam.to,Mycima and you can also create IPTV and search any m3u_plus",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: Python :: 3",
+	"Operating System :: OS Independent",
+	"License :: OSI Approved :: MIT License"
+	]
+)
